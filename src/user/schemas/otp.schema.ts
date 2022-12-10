0a45e8387e11ffed1125b953024dc857d8c3e8c1
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-12-10 14:49:59 +0100

    created schema for otps

---
+++ src/user/schemas/otp.schema.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, IsOptional, IsPhoneNumber, Length } from 'class-validator';
+import { Document } from 'mongoose';
+import { REGISTEROPTIONS, USERROLES } from '../../utils/enum';
+
+export type OtpDocument = Otp & Document;
+
+@Schema({
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        transform(doc, ret) {
+            delete ret._id;
+            delete ret._v
+        }
+    },
+    toObject: {
+        virtuals: true,
+        transform(doc, ret) {
+            doc.id = ret._id;
+        }
+    }
+})
+export class Otp {
+    @Prop({ required: true })
+    @IsEmail()
+    email: string;
+
+    @Prop({ required: true })
+    otp: string;
+
+    @Prop({ required: true })
+    type: string;
+
+    @Prop()
+    usage: string;
+
+    @Prop()
+    expiry: string
+
+    @Prop()
+    status: string;
+}
+
+
+
+export const OtpSchema = SchemaFactory.createForClass(Otp);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,12 +8,13 @@ import { HashService } from './hash.service';
 import { AuthService } from '../auth/auth.service';
 import { ConfigService } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
+import { Otp, OtpSchema } from './schemas/otp.schema';
 
 
 @Global()
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema },{name:Otp.name, schema:OtpSchema}]),
     AuthModule
   ],
   providers: [UserService, HashService, AuthService,ConfigService],
