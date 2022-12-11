33bb0f631b1ec200c9d658e8a8666f9576bfd1bd
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-12-11 09:28:57 +0100

    configure twilio setup

--- src/sms/sms.controller.ts
+++ src/sms/sms.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { SmsService } from './sms.service';
+import { Request } from '../client/request';
 
 @Controller('sms')
-export class SmsController {}
+export class SmsController {
+    constructor(
+        private readonly smsService: SmsService
+    ) { }
+
+    @Post('initiate-verification')
+    @UseGuards(AuthGuard('jwt'))
+    async initiatePhoneNumberVerification(@Req() req: Request) {
+        if (req.user.isPhoneNumberVerified) {
+            throw new BadRequestException('Phone number already confirmed');
+        }
+        await this.smsService.initiatePhoneNumberVerification(req.user.phoneNumber);
+    }
+}
--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Twilio } from 'twilio';
 
 @Injectable()
-export class SmsService {}
+export class SmsService {
+    private twilioClient: Twilio;
+
+    constructor(
+        
+    ) {
+        const accountSid = process.env.TWILO_ACCOUNT_SID;
+        const authToken = process.env.TWILO_AUTH_TOKEN;
+
+        this.twilioClient = new Twilio(accountSid, authToken);
+    }
+
+    initiatePhoneNumberVerification(phoneNumber: string) {
+        const serviceSid = process.env.TWILIO_VERIFICATION_SERVICE_SID;
+
+        return this.twilioClient.verify.services(serviceSid)
+            .verifications
+            .create({ to: phoneNumber, channel: 'sms' })
+    }
+}
